423d1284c8127b10c306f90eacb107c81c9baf6d
Bogdan Livadariu <bogdan.livadariu@gmail.com>
2017-04-15 02:43:26 +0300

    oops :)

--- frontend/src/services/api-service.js
+++ frontend/src/services/api-service.js
@@ -4,8 +4,8 @@ import { HttpClient } from 'aurelia-http-client';
 @singleton()
 export class ApiService {
     constructor() {
-        const backendURL = 'http://localhost:4040/api/';
-        // const backendURL = 'https://be-leave-tracking.herokuapp.com/api/';
+        // const backendURL = 'http://localhost:4040/api/';
+        const backendURL = 'https://be-leave-tracking.herokuapp.com/api/';
 
         this.httpClient = new HttpClient().configure(x => {
             x.withHeader('Content-Type', 'application/json');
