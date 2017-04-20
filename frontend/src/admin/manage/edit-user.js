a39a2c2bade007e1e55f2609c24e9b0d0c7259cb
Bogdan Livadariu <bogdan.livadariu@gmail.com>
2017-04-20 10:11:17 +0300

    added missing templates

---
+++ frontend/src/admin/manage/edit-user.js
@@ -0,0 +1,23 @@
+import { inject } from 'aurelia-framework';
+import { UserService } from '../../services/user-service';
+import { ProjectService } from '../../services/project-service';
+
+@inject(UserService, ProjectService)
+export class EditUser {
+    constructor(_user, _project) {
+        this._user = _user;
+        this._project = _project;
+    }
+
+    async activate(params) {
+        console.log('the params', params)
+        const user = await this._user.getUser(params.userId);
+        const projects = await this._project.getProjects();
+
+        console.log('the user', user);
+        this.user = user;
+        this.projects = projects;
+        console.log(projects)
+    }
+
+}
