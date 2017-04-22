4660428b16dc231faf038ba953365290d72f8132
Bogdan Livadariu <bogdan.livadariu@gmail.com>
2017-04-22 13:14:08 +0300

    display legal holidays on calendar

--- frontend/src/calendar.js
+++ frontend/src/calendar.js
@@ -1,50 +1,56 @@
 import { inject } from 'aurelia-framework';
+import moment from 'moment';
 import { LeaveService } from '~/services/leave-service'
+import { HolidayService } from '~/services/holiday-service'
 import { HUMAN_LEAVE_TYPES } from '~/util/constants';
 
-@inject(LeaveService)
+@inject(LeaveService, HolidayService)
 export class Calendar {
     isLoading = true;
     calendar ={}
-    constructor(leaveService) {
-        this.leaveService = leaveService;
+    config = {
+        tmpl_path: "bootstrap-calendar/tmpls/",
+        events_source: {},
+        modal_type: 'template',
+        tmpl_cache: true,
+        modal: '#events-modal',
+        modal_title: function(ev) {
+            return ev.title;
+        },
+        view: 'month',
+        weekbox: false,
+        views: {
+            day: {
+                enable: 0
+            }
+        },
+        merge_holidays: false,
+        holidays: {},
+        onAfterViewLoad: function(view) {
+            $('.page-header h3').text(this.getTitle());
+            $('.btn-group button').removeClass('active');
+            $('button[data-calendar-view="' + view + '"]').addClass('active');
+        }
+    }
+
+    constructor(_leave, _holiday) {
+        this._leave = _leave;
+        this._holiday = _holiday;
     }
 
     async attached() {
-        const events = await this.leaveService.getCalendarEvents()
+        const events = await this._leave.getCalendarEvents();
+        const holidays = await this._holiday.getHolidays();
 
-        this.displayCalendar(events)
+        this.setEventsToConfig(events);
+        this.setHolidaysToConfig(holidays);
+
+        this.displayCalendar();
         this.isLoading = false;
     }
 
-    displayCalendar(events) {
-        this.calendar = $('#calendar').calendar(
-            {
-                tmpl_path: "bootstrap-calendar/tmpls/",
-                events_source: events,
-                modal_type: 'template',
-                tmpl_cache: true,
-                modal: '#events-modal',
-                modal_title: function(ev) {
-                    return ev.title;
-                },
-                view: 'month',
-                weekbox: false,
-                views: {
-                    day: {
-                        enable: 0
-                    }
-                },
-                merge_holidays: false,
-                holidays: {
-                },
-                onAfterViewLoad: function(view) {
-                    $('.page-header h3').text(this.getTitle());
-                    $('.btn-group button').removeClass('active');
-                    $('button[data-calendar-view="' + view + '"]').addClass('active');
-                }
-            }
-      );
+    displayCalendar() {
+        this.calendar = $('#calendar').calendar(this.config);
     }
 
     switchViewTo(view) {
@@ -52,5 +58,21 @@ export class Calendar {
     }
     navigateTo(nav) {
         this.calendar.navigate(nav);
+    }
+
+    setEventsToConfig(events) {
+        this.config.events_source = events
+    }
+
+    setHolidaysToConfig(holidays) {
+        const legalHolidays = {};
+
+        holidays.forEach(h => {
+            const date = moment(h.date).format('DD-MM');
+            legalHolidays[date] = h.name;
+
+        });
+
+        this.config.holidays = legalHolidays;
     }
 }
