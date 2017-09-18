e827227148ea436296b4e2181e81f8bdd6c0d34e
Bogdan Livadariu <bogdan.livadariu@gmail.com>
2017-09-18 13:55:19 +0300

    added view for forgot/recover password

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -92,6 +92,16 @@ export class App {
                 }
             },
             {
+                route: 'recover/:recoverKey?',
+                name: 'recover',
+                moduleId: './recover-password/recover-password',
+                nav: false,
+                title:'Recover Password !',
+                settings: {
+                },
+                auth: false
+            },
+            {
                 route: 'profile',
                 name: 'profile',
                 moduleId: './profile/profile',
@@ -115,13 +125,16 @@ class AuthorizeStep {
         this.router = router;
 
         this._events.ea.subscribe('no_token', (rr) => {
-            this.router.navigateToRoute('login')
+            if (['login', 'recover'].indexOf(this.router.currentInstruction.config.name) == -1) {
+                this.router.navigateToRoute('login')
+            }
         })
     }
 
   run(navigationInstruction, next) {
     if (navigationInstruction.getAllInstructions().some(i => i.config.auth)) {
       var isLoggedIn = this._auth.isAuth;
+
       if (!isLoggedIn) {
         return next.cancel(new Redirect('login'));
       }
