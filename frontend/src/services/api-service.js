ee02798497a55204706c17d71d471d9702b0b0ad
Bogdan Livadariu <bogdan.livadariu@gmail.com>
2017-09-26 23:09:01 +0300

    remove code duplication a bit

--- frontend/src/services/api-service.js
+++ frontend/src/services/api-service.js
@@ -8,19 +8,15 @@ export class ApiService {
     constructor(_notify) {
         // const backendURL = 'http://localhost:4040/api/';
         const backendURL = 'https://be-leave-tracking.herokuapp.com/api/';
+        const that = this;
+        this._notify = _notify;
 
         this.httpClient = new HttpClient().configure(x => {
             x.withHeader('Content-Type', 'application/json');
             x.withBaseUrl(backendURL);
             x.withInterceptor({
-                requestError(error) {
-                    _notify.danger(`${error.statusCode}|${error.statusText}`, { timeout: 0 });
-                    return Promise.reject(new Error(error));
-                },
-                responseError(error) {
-                    _notify.danger(`${error.statusCode}|${error.statusText}`, { timeout: 0 });
-                    return Promise.reject(new Error(error));
-                }
+                requestError(error) { return that.handleError(error); },
+                responseError(error) { return that.handleError(error); }
             });
         });
 
@@ -39,5 +35,10 @@ export class ApiService {
         this.httpClient.configure(config => {
             config.withHeader('Authorization', `Bearer ${token}`)
         })
+    }
+
+    handleError(error) {
+        this._notify.danger(`${error.statusCode} | ${error.statusText}`, { timeout: 0 });
+        return Promise.reject(new Error(error.statusText));
     }
 }
