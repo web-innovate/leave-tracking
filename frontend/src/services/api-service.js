311bbd4b79b243728ab342bfe98535e5bc7c7170
Bogdan Livadariu <bogdan.livadariu@gmail.com>
2017-09-26 22:58:05 +0300

    in case BE plays dumb, show a red notification so you know :)

--- frontend/src/services/api-service.js
+++ frontend/src/services/api-service.js
@@ -1,9 +1,11 @@
-import { singleton } from 'aurelia-framework'
+import { singleton, inject } from 'aurelia-framework'
 import { HttpClient } from 'aurelia-http-client';
+import { NotificationService } from 'aurelia-notify';
 
 @singleton()
+@inject(NotificationService)
 export class ApiService {
-    constructor() {
+    constructor(_notify) {
         // const backendURL = 'http://localhost:4040/api/';
         const backendURL = 'https://be-leave-tracking.herokuapp.com/api/';
 
@@ -12,9 +14,11 @@ export class ApiService {
             x.withBaseUrl(backendURL);
             x.withInterceptor({
                 requestError(error) {
+                    _notify.danger(`${error.statusCode}|${error.statusText}`, { timeout: 0 });
                     return Promise.reject(new Error(error));
                 },
                 responseError(error) {
+                    _notify.danger(`${error.statusCode}|${error.statusText}`, { timeout: 0 });
                     return Promise.reject(new Error(error));
                 }
             });
