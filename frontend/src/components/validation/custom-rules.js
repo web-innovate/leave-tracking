73cb8139ac70254a5c783a47826cb113ea1fa1f6
Mircea Sinteanu <mircea.sinteanu@gmail.com>
2017-10-17 11:51:57 +0300

    - updated custom rule function syntax

--- frontend/src/components/validation/custom-rules.js
+++ frontend/src/components/validation/custom-rules.js
@@ -2,13 +2,13 @@ import { ValidationRules } from 'aurelia-validation';
 
 export function setupCustomValidationRules() {
     const integerRangeMessage = `\${$displayName} must be an integer between \${$config.min} and \${$config.max}.`;
-    const integerRangeRule = function(value, obj, min, max) {
+    const integerRangeRule = (value, obj, min, max) => {
         return value === null || value === undefined
             || Number.isInteger(parseInt(value)) && parseInt(value) >= min && parseInt(value) <= max;
     };
 
     const otherThanMessage = `\${$displayName} must be selected.`;
-    const otherThanRule = function(value, obj, str) {
+    const otherThanRule = (value, obj, str) => {
         return value === null || value === undefined || value !== str;
     };
 
