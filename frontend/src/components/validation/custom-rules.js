e0d107d658f188df0cd710b91c73c4bce0f7dde8
Mircea Sinteanu <mircea.sinteanu@gmail.com>
2017-10-17 11:31:59 +0300

    - passed rules and messages as params to ValidationRules config

--- frontend/src/components/validation/custom-rules.js
+++ frontend/src/components/validation/custom-rules.js
@@ -1,16 +1,26 @@
 import { ValidationRules } from 'aurelia-validation';
 
 export function setupCustomValidationRules() {
+    const integerRangeMessage = `\${$displayName} must be an integer between \${$config.min} and \${$config.max}.`;
+    const integerRangeRule = function(value, obj, min, max) {
+        return value === null || value === undefined
+            || Number.isInteger(parseInt(value)) && parseInt(value) >= min && parseInt(value) <= max;
+    };
+
+    const otherThanMessage = `\${$displayName} must be selected.`;
+    const otherThanRule = function(value, obj, str) {
+        return value === null || value === undefined || value !== str;
+    };
+
     ValidationRules.customRule(
         'integerRange',
-        (value, obj, min, max) => value === null || value === undefined
-            || Number.isInteger(parseInt(value)) && parseInt(value) >= min && parseInt(value) <= max,
-        `\${$displayName} must be an integer between \${$config.min} and \${$config.max}.`,
+        integerRangeRule,
+        integerRangeMessage,
         (min, max) => ({ min, max })
     );
     ValidationRules.customRule(
         'otherThan',
-        (value, obj, str) => value === null || value === undefined || value !== str,
-        `\${$displayName} must be selected.`
+        otherThanRule,
+        otherThanMessage
     );
 }
