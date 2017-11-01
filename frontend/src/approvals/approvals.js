9d11801d0be70b026e098abd31b4f9baa5e3a9c6
Bogdan Livadariu <bogdan.livadariu@gmail.com>
2017-11-01 13:00:41 +0200

    approvers will see requests that are assigned to their project, and they will be grouped

--- frontend/src/approvals/approvals.js
+++ frontend/src/approvals/approvals.js
@@ -1,45 +1,58 @@
 import { inject } from 'aurelia-framework';
-import { HttpClient } from 'aurelia-http-client';
 import { LeaveService } from '~/services/leave-service';
 import { UserService } from '~/services/user-service';
-import { AuthService } from '~/services/auth-service';
 import { REQUEST_STATUS } from '~/util/constants';
 
 @inject(LeaveService, UserService)
 export class Approvals {
+    pendingLoading = true;
+    approvedLoading = true;
+    rejectedLoading = true;
+
     constructor(_leave, _user) {
         this._leave = _leave;
         this._user = _user;
     }
     activate() {
-        this.pendingApprovals();
+        this.pendingRequests();
+        this.approvedRequests();
+        this.rejectedRequests();
     }
 
-    async pendingApprovals() {
-        const pendings = await this._leave.getPendingApprovals();
-        const complete = await Promise.all(pendings.map(async item => {
+    async pendingRequests() {
+        let pendingsRequests = await this._leave.getPendingRequests();
+
+        pendingsRequests = await this.agregateUserData(pendingsRequests);
+
+        this.allPendingRequests = pendingsRequests;
+        this.pendingLoading = false;
+    }
+
+
+    async approvedRequests() {
+        let approvedRequests = await this._leave.getApprovedRequests();
+
+        approvedRequests = await this.agregateUserData(approvedRequests);
+
+        this.allApprovedRequests = approvedRequests;
+        this.approvedLoading = false;
+    }
+
+    async rejectedRequests() {
+        let rejectedRequests = await this._leave.getRejectedRequests();
+
+        rejectedRequests = await this.agregateUserData(rejectedRequests);
+
+        this.allRejectedRequests = rejectedRequests;
+        this.rejectedLoading = false;
+    }
+
+    agregateUserData(pendings) {
+        return Promise.all(pendings.map(async item => {
             const user = await this._user.getUser(item.userId);
 
             item.user = user;
             return item;
         }));
-
-        this.allPendingApprovals = complete;
-    }
-
-    async approveRequest(request) {
-        const response = await this._leave.updateLeaveRequestStatus(request, REQUEST_STATUS.APPROVED);
-        console.log('approve', response)
-        return response;
-    }
-
-    async rejectRequest(request) {
-        const response = await this._leave.updateLeaveRequestStatus(request, REQUEST_STATUS.REJECTED);
-        console.log('reject', response)
-        return response;
-    }
-
-    showExtra(extra) {
-        return extra.workDays > 1 ? true : false;
     }
 }
---
+++ frontend/src/approvals/requests.js
@@ -0,0 +1,25 @@
+import { bindable, inject } from 'aurelia-framework';
+import { LeaveService } from '~/services/leave-service';
+import { REQUEST_STATUS } from '~/util/constants';
+
+@inject(LeaveService)
+export class Requests {
+  @bindable requests = [];
+  @bindable loading = false;
+
+  constructor(_leave){
+    this._leave = _leave;
+  }
+
+    approveRequest(request) {
+        return this._leave.updateLeaveRequestStatus(request, REQUEST_STATUS.APPROVED);
+    }
+
+    rejectRequest(request) {
+        return this._leave.updateLeaveRequestStatus(request, REQUEST_STATUS.REJECTED);
+    }
+
+    showExtra(extra) {
+        return extra.workDays > 1 ? true : false;
+    }
+}
