3b0021b0ad455c3eabdd16868b04ab5390223b5a
Bogdan Livadariu <bogdan.livadariu@gmail.com>
2017-11-02 09:22:23 +0200

    indents

--- backend/server/controllers/project.controller.js
+++ backend/server/controllers/project.controller.js
@@ -4,12 +4,12 @@ import APIError from '../helpers/APIError';
 
 function load(req, res, next, id) {
     Project.get(id)
-    .then((project) => {
-        req.project = project;
-        next();
-        return null;
-    })
-    .catch(e => next(e));
+        .then((project) => {
+            req.project = project;
+            next();
+            return null;
+        })
+        .catch(e => next(e));
 }
 
 function get(req, res) {
@@ -19,6 +19,7 @@ function get(req, res) {
 async function create(req, res, next) {
     const project = new Project({
         approvers: req.body.approvers,
+        roles: req.body.roles,
         name: req.body.name,
         description: req.body.description,
     });
@@ -32,19 +33,20 @@ function update(req, res, next) {
     const project = req.project;
     project.approvers = req.body.approvers;
     project.name = req.body.name;
+    project.roles = req.body.roles;
     project.description = req.body.description;
 
 
     project.save()
-    .then(savedProject => res.json(savedProject))
-    .catch(e => next(e));
+        .then(savedProject => res.json(savedProject))
+        .catch(e => next(e));
 }
 
 function list(req, res, next) {
     const { limit = 50, skip = 0 } = req.query;
     Project.list({ limit, skip })
-    .then(projects => res.json(projects))
-    .catch(e => next(e));
+        .then(projects => res.json(projects))
+        .catch(e => next(e));
 }
 
 async function remove(req, res, next) {
--- backend/server/models/project.model.js
+++ backend/server/models/project.model.js
@@ -17,6 +17,10 @@ const ProjectSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    roles: {
+        type: [ String ],
+        required: true
+    },
     createdAt: {
         type: Date,
         default: Date.now
