0d793a1aa7ed1b0aa87204816755668a305b48b6
Bogdan Livadariu <bogdan.livadariu@gmail.com>
2018-05-15 20:13:41 +0300

    prevent worker crashes

--- backend/worker/users/userWorkers.js
+++ backend/worker/users/userWorkers.js
@@ -6,25 +6,35 @@ import { UserSchema } from '../../server/models/user.model';
 const User = mongoose.model('User', UserSchema);
 
 function handleNewUsers(params, callback) {
-    const { email, firstName } = params;
-    const emailSubject = `${firstName} your account has been created`;
+    try {
+        const { email, firstName } = params;
+        const emailSubject = `${firstName} your account has been created`;
+        const domain = process.env.FE_DOMAIN || 'http://localhost:3000';
 
-    smtp.sendMail(email, emailSubject, 'newUser', params)
-        .then(info => callback(null, info))
-        .catch(err => callback(err));
+        params.domain = domain;
+
+        smtp.sendMail(email, emailSubject, 'newUser', params)
+            .then(info => callback(null, info))
+    } catch(error) {
+        console.log('err handleNewUsers', error)
+        return callback(error);
+    }
 }
 
 function handlePasswordReset(params, callback) {
-    const { email, firstName } = params.user;
-    const fe_domain = process.env.fe_domain || 'http://localhost:9000';
+    try {
+        const { email, firstName } = params.user;
+        const fe_domain = process.env.FE_DOMAIN || 'http://localhost:3000';
 
-    params.domain = fe_domain;
+        params.domain = fe_domain;
 
-    const emailSubject = `${firstName} password reset`;
+        const emailSubject = `${firstName} password reset`;
 
-    smtp.sendMail(email, emailSubject, 'resetPassword', params)
-        .then(info => callback(null, info))
-        .catch(err => callback(err));
+        smtp.sendMail(email, emailSubject, 'resetPassword', params)
+            .then(info => callback(null, info))
+    } catch(error) {
+        return callback(error);
+    }
 }
 
 function createDefaultUser(params, cb) {
