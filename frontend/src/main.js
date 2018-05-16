d927ab7f78aa8655e4175a24873355d8cee8862b
Bogdan Livadariu <bogdan.livadariu@gmail.com>
2018-05-16 20:56:54 +0300

    add event tracking in the app

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -15,6 +15,45 @@ export function configure(aurelia) {
         .plugin('aurelia-notify', settings => {
           settings.timeout = 4000;
         })
+        .plugin('aurelia-google-analytics', config => {
+            config.init('UA-118670465-1');
+            config.attach({
+                logging: {
+                    // Set to `true` to have some log messages appear in the browser console.
+                    enabled: true
+                },
+                pageTracking: {
+                    // Set to `false` to disable in non-production environments.
+                    enabled: true,
+                    // Optional. By default it gets the title from payload.instruction.config.title.
+                    getTitle: (payload) => {
+                        // For example, if you want to retrieve the tile from the document instead override with the following.
+                        return document.title;
+                    },
+                    // Optional. By default it gets the URL fragment from payload.instruction.fragment.
+                    getUrl: (payload) => {
+                    // For example, if you want to get full URL each time override with the following.
+                    return window.location.href;
+                    }
+                },
+                clickTracking: {
+                    // Set to `false` to disable in non-production environments.
+                    enabled: true,
+                    // Optional. By default it tracks clicks on anchors and buttons.
+                    filter: (element) => {
+                        // For example, if you want to also track clicks on span elements override with the following.
+                        return element instanceof HTMLElement &&
+                        (element.nodeName.toLowerCase() === 'a' ||
+                        element.nodeName.toLowerCase() === 'button' ||
+                        element.nodeName.toLowerCase() === 'span');
+                    }
+                },
+                exceptionTracking: {
+                    // Set to `false` to disable in non-production environments.
+                    enabled: true
+                }
+            });
+        })
         .feature('resources');
 
     if (environment.debug) {
